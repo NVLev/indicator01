4ef3af14ab183a8189f7e9b78f333e34b1c894e0
natalia_levant <an_smir@mail.ru>
2025-09-21 16:15:34 +0300

    Созданы модели и схемы для системы авторизации/аутентификации, настроено подключение к БД, настроен alembic, обновлена база данных

---
+++ backend/app/core/schemas.py
@@ -0,0 +1,38 @@
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8010
+
+
+class DatabaseConfig(BaseModel):
+    url: str = "postgresql+asyncpg://user:password@pg:5432/eksmo"
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 10
+
+
+class AuthConfig(BaseModel):
+    secret_key: str = "super-secret-key"
+    algorithm: str = "HS256"
+    ACCESS_EXPIRE_MINUTES: int = 15
+    REFRESH_EXPIRE_DAYS: int = 7
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig = DatabaseConfig()
+    auth: AuthConfig = AuthConfig()
+
+
+settings = Settings()
